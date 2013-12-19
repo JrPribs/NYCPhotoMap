f688c958be8066ddd21d00de7be5682c2b6c8196
John Pribesh <beopensourcellc@gmail.com>
2013-12-19 00:24:07 -0500

    Updating files

--- js/picMap.js
+++ js/picMap.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    function buildMap(stations) {
+    function loopIt(stations) {
         var stationed = [];
         for(var i=0; i<stations.length; i++){
         var loc = stations[i].loc;
@@ -9,23 +9,19 @@ $(document).ready(function(){
         }
         return stationed.join('\n');
     }
-    function makeInfo(inf){
-        var info = [];
-        for(var i=0; i<imgs.length; i++){
-            info.push(inf);
-        }
-        return info.join(',');
-    }
-    $('#subway').append(buildMap(stations));
-    $('#zoom').bezoom();
+    $('#subway').append(loopIt(stations));
     $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false});
     $('img[usemap]').rwdImageMaps();
     $('a').on('click', function(e){
         e.preventDefault();
         var ib = $(this).data('i');
-        var imgs = stations[ib].imgName;
-        var name = makeInfo(stations[ib].name);
-        var desc = makeInfo(name + ' - Lines running to this stop: ' + stations[ib].line);
+        imgs = stations[ib].imgName;
+        var name = [];
+        var desc = [];
+        for(var i=0; i<imgs.length; i++){
+            name.push(stations[ib].name);
+            desc.push(name + ' - Lines running to this stop: ' + stations[ib].line);
+        }
         $.prettyPhoto.open(imgs,name,desc);
     });
 })
